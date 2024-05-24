e663197ecf0726c589f59c97521f20df3e4cf7bc
Miguel Angel <155111126+pirry007@users.noreply.github.com>
2024-05-23 21:50:50 -0500

    product bases and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { CatalogueComponent } from './domains/catalogue/catalogue.component';
+import { HomeComponent } from './domains/home/home.component';
+import { ProductComponent } from './domains/product/product.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "catalogue", component: CatalogueComponent},
+    {path: "", component: HomeComponent},
+    {path: "product", component: ProductComponent}
+];
