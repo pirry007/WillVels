cc3341916755c5ff9f330a2049c4f8d730d1fdc6
Miguel Angel <155111126+pirry007@users.noreply.github.com>
2024-06-04 20:33:56 -0500

    added footer basic css

--- src/app/domains/home/home.component.ts
+++ src/app/domains/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../component/header/header.component';
+import { FooterComponent } from '../../component/footer/footer.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent,FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
