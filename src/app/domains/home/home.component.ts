f98cb4ea46b1bed55d95a8b8addae51360485552
Miguel Angel <155111126+pirry007@users.noreply.github.com>
2024-06-04 21:30:10 -0500

    minor changes in homepage

--- src/app/domains/home/home.component.ts
+++ src/app/domains/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from '../../component/header/header.component';
-import { FooterComponent } from '../../component/footer/footer.component';
+import { HeaderComponent } from '../../components/header/header.component';
+import { FooterComponent } from '../../components/footer/footer.component';
+import { CardProductComponent } from '../../components/card-product/card-product.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent,FooterComponent],
+  imports: [HeaderComponent,FooterComponent,CardProductComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
