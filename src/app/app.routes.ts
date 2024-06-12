09d5b02b3d5a498f855ef2dfc0cf5e41b9f32688
Miguel Angel <155111126+pirry007@users.noreply.github.com>
2024-06-11 22:30:30 -0500

    Login and Register pages created (basic)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,13 @@ import { Routes } from '@angular/router';
 import { CatalogueComponent } from './domains/catalogue/catalogue.component';
 import { HomeComponent } from './domains/home/home.component';
 import { ProductComponent } from './domains/product/product.component';
+import { LoginComponent } from './domains/login/login.component';
+import { RegisterComponent } from './domains/register/register.component';
 
 export const routes: Routes = [
     {path: "catalogue", component: CatalogueComponent},
     {path: "", component: HomeComponent},
-    {path: "product", component: ProductComponent}
+    {path: "product", component: ProductComponent},
+    {path: "login", component: LoginComponent},
+    {path: "register", component: RegisterComponent}
 ];
